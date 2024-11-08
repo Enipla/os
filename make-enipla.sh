40e4214640d10aec36ad84d1a5994a6f8c4870d0
Sectly <67114413+Sectly@users.noreply.github.com>
2024-11-08 12:33:31 +0100

    Update make-enipla.sh

--- make-enipla.sh
+++ make-enipla.sh
@@ -1,78 +1,59 @@
 #!/bin/bash
 
-# Script to set up and build Enipla Linux from Alpine Linux 3.20-stable
+# Script to customize and build Enipla Linux ISO from Alpine Linux 3.20-stable
 
-# Exit if any command fails
+# Exit immediately if any command fails
 set -e
 
 # Variables
-REPO_URL="https://github.com/Enipla/aports.git"
+REPO_PATH="aports"  # Path to your cloned Enipla aports repo
 BRANCH="3.20-stable"
 ISO_OUTPUT_DIR="/tmp/enipla-output"
 ISO_NAME="enipla.iso"
-LOGO_PATH="./enipla_logo_brand.png"
+LOGO_PATH="./hd_enipla_logo_icon_transparent.png"
 BACKGROUND_PATH="./enipla_background.png"
-ICON_PATH="./enipla_logo_icon.png"
-ICON_TRANSPARENT_PATH="./enipla_logo_icon_transparent.png"
-HD_LOGO_PATH="./hd_enipla_logo_brand.png"
-HD_LOGO_TRANSPARENT_PATH="./hd_enipla_logo_brand_transparent.png"
-HD_ICON_PATH="./hd_enipla_logo_icon.png"
-HD_ICON_TRANSPARENT_PATH="./hd_enipla_logo_icon_transparent.png"
 CUSTOM_NAME="Enipla"
 
-# Ensure necessary dependencies are installed for building
-echo "Installing necessary dependencies..."
-sudo apt update
-sudo apt install -y git curl squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin
-
-# Clone the repository
-echo "Cloning Enipla repository... (Enipla/aports)"
-git clone $REPO_URL
-cd aports
-
-# Checkout the 3.20-stable branch
-echo "Switching to branch $BRANCH..."
+# Step 1: Clone the repository and switch to the 3.20-stable branch if not already cloned
+if [ ! -d "$REPO_PATH" ]; then
+    echo "Cloning Enipla repository..."
+    git clone https://github.com/Enipla/aports.git "$REPO_PATH"
+fi
+cd "$REPO_PATH"
 git checkout $BRANCH
 
-# Update OS Name in os-release
-echo "Updating OS name in os-release..."
-echo "NAME=\"$CUSTOM_NAME\"" | sudo tee rootfs/etc/os-release
-echo "PRETTY_NAME=\"$CUSTOM_NAME 3.20\"" | sudo tee -a rootfs/etc/os-release
+# Step 2: Modify mkimage.sh to include custom branding and software packages
+MKIMAGE_SCRIPT="scripts/mkimage.sh"
+echo "Customizing $MKIMAGE_SCRIPT..."
 
-# Branding Customizations
-# 1. Change Login Logo and Icons
-echo "Setting custom logos and icons..."
-sudo cp "$LOGO_PATH" rootfs/usr/share/pixmaps/enipla_logo_brand.png
-sudo cp "$ICON_PATH" rootfs/usr/share/icons/enipla_logo_icon.png
-sudo cp "$ICON_TRANSPARENT_PATH" rootfs/usr/share/icons/enipla_logo_icon_transparent.png
-sudo cp "$HD_LOGO_PATH" rootfs/usr/share/pixmaps/hd_enipla_logo_brand.png
-sudo cp "$HD_LOGO_TRANSPARENT_PATH" rootfs/usr/share/pixmaps/hd_enipla_logo_brand_transparent.png
-sudo cp "$HD_ICON_PATH" rootfs/usr/share/icons/hd_enipla_logo_icon.png
-sudo cp "$HD_ICON_TRANSPARENT_PATH" rootfs/usr/share/icons/hd_enipla_logo_icon_transparent.png
+# Backup original mkimage.sh
+cp "$MKIMAGE_SCRIPT" "$MKIMAGE_SCRIPT.bak"
 
-# Update LightDM or other login managers to use the new background
-LIGHTDM_CONF="rootfs/etc/lightdm/lightdm-gtk-greeter.conf"
-if [ -f "$LIGHTDM_CONF" ]; then
-    echo "Setting LightDM background..."
-    sudo sed -i "s|#background=.*|background=$BACKGROUND_PATH|" "$LIGHTDM_CONF"
-fi
+# Add branding and default software packages
+sed -i "/^PRETTY_NAME=/c\PRETTY_NAME=\"$CUSTOM_NAME 3.20\"" "$MKIMAGE_SCRIPT"
 
-# 2. Set Custom Desktop Background
-echo "Setting custom desktop background..."
-sudo mkdir -p rootfs/usr/share/backgrounds
-sudo cp "$BACKGROUND_PATH" rootfs/usr/share/backgrounds/enipla_background.png
-# Configure XFCE to use the custom background by default
-XFCE_DESKTOP_CONF="rootfs/usr/share/xfce4/backdrops/default.png"
-sudo ln -sf /usr/share/backgrounds/enipla_background.png "$XFCE_DESKTOP_CONF"
+# Insert software packages and branding assets at an appropriate point in the script
+sed -i "/^apk add --no-cache/ s/$/ xfce4 xfce4-terminal thunar firefox mousepad vlc evince ristretto file-roller networkmanager networkmanager-applet htop gparted xfce4-power-manager lightdm/" "$MKIMAGE_SCRIPT"
 
-# Build the ISO
+# Add branding assets
+echo "cp $LOGO_PATH rootfs/usr/share/pixmaps/" >> "$MKIMAGE_SCRIPT"
+echo "cp $BACKGROUND_PATH rootfs/usr/share/backgrounds/" >> "$MKIMAGE_SCRIPT"
+echo "echo \"background=/usr/share/backgrounds/enipla_background.png\" >> /etc/lightdm/lightdm-gtk-greeter.conf" >> "$MKIMAGE_SCRIPT"
+
+# Step 3: Ensure LightDM and NetworkManager are enabled on boot
+echo "Configuring LightDM and NetworkManager to start on boot..."
+echo "rc-update add lightdm" >> "$MKIMAGE_SCRIPT"
+echo "rc-update add NetworkManager" >> "$MKIMAGE_SCRIPT"
+
+# Step 4: Run mkimage.sh to build the ISO
 echo "Building the ISO..."
 mkdir -p "$ISO_OUTPUT_DIR"
-./scripts/mkimage.sh --profile standard --outdir "$ISO_OUTPUT_DIR" --arch x86_64
+chmod +x "$MKIMAGE_SCRIPT"
+./"$MKIMAGE_SCRIPT" --profile standard --outdir "$ISO_OUTPUT_DIR" --arch x86_64
 
-# Rename and move the ISO
+# Step 5: Rename and move the ISO
 ISO_PATH="$ISO_OUTPUT_DIR/$ISO_NAME"
-mv "$ISO_OUTPUT_DIR"/alpine-standard-*.iso "$ISO_PATH"
+mv "$ISO_OUTPUT_DIR/alpine-standard-*.iso" "$ISO_PATH"
 
 echo "ISO created at $ISO_PATH"
 echo "Enipla build complete!"

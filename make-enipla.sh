ab7a1c6e548441b79ed1e9a83d4d4ab181b14fe1
Sectly <67114413+Sectly@users.noreply.github.com>
2024-11-08 15:05:22 +0100

    Update make-enipla.sh

--- make-enipla.sh
+++ make-enipla.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to set up a proot Alpine environment, customize Enipla Linux, and build the ISO from Alpine Linux 3.20-stable
+# Script to set up an Alpine VM image with customizations for Enipla Linux, including a desktop environment
 
 # Exit immediately if any command fails
 set -e
@@ -8,121 +8,89 @@ set -e
 # Install dependencies on Debian/Ubuntu
 echo "Installing dependencies..."
 sudo apt update
-sudo apt install -y proot wget git xorriso curl
+sudo apt install -y proot wget git xorriso curl openssl qemu-utils
 
 # Variables
 OS_REPO="https://github.com/Enipla/os"  # Repository containing OS files (images, etc.)
-REPO_PATH="aports"  # Path to your cloned Enipla aports repo
-BRANCH="3.20-stable"
 ISO_OUTPUT_DIR="/tmp/enipla-output"
 ISO_NAME="enipla.iso"
 CUSTOM_NAME="Enipla"
-ALPINE_VERSION="3.20.3"
-ALPINE_ROOT="alpine-root"
-REPOSITORY_URL="http://dl-cdn.alpinelinux.org/alpine/v3.20/main"  # Specify the Alpine repository
-UPDATE_KERNEL_URL="https://raw.githubusercontent.com/alpinelinux/alpine-conf/refs/heads/master/update-kernel.in"  # URL for update-kernel script
+REPOSITORY_URL="http://dl-cdn.alpinelinux.org/alpine"  # Base URL for the Alpine repository
+ALPINE_MAKE_VM_IMAGE_URL="https://raw.githubusercontent.com/alpinelinux/alpine-make-vm-image/refs/heads/master/alpine-make-vm-image"
 
-# Step 1: Download and extract Alpine miniroot filesystem
-if [ ! -d "$ALPINE_ROOT" ]; then
-    echo "Setting up Alpine environment..."
-    wget http://mirror.nl.leaseweb.net/alpine/v3.20/releases/x86_64/alpine-minirootfs-${ALPINE_VERSION}-x86_64.tar.gz
-    mkdir -p "$ALPINE_ROOT"
-    sudo tar -xzf alpine-minirootfs-${ALPINE_VERSION}-x86_64.tar.gz -C "$ALPINE_ROOT"
+# Generate signing keys
+echo "Generating signing keys..."
+openssl req -new -x509 -days 3650 -nodes -out enipla-cert.pem -keyout enipla-key.pem -subj "/CN=Enipla Kernel Signing"
+
+# Download alpine-make-vm-image tool
+if [ ! -f "alpine-make-vm-image" ]; then
+    echo "Downloading alpine-make-vm-image..."
+    wget -O alpine-make-vm-image $ALPINE_MAKE_VM_IMAGE_URL
+    chmod +x alpine-make-vm-image
 fi
 
-# Step 2: Start Alpine environment with proot
-echo "Entering Alpine environment with proot..."
-proot -R "$ALPINE_ROOT" /bin/sh -c "
-    # Inside proot Alpine environment
-    apk update
-    apk add alpine-sdk git bash xorriso openrc lightdm networkmanager abuild apk-tools syslinux
+# Clone the OS resources repository
+if [ ! -d 'os' ]; then
+    echo 'Cloning Enipla OS repository...'
+    git clone $OS_REPO os
+fi
 
-    # Download update-kernel script and make it executable
-    echo 'Downloading update-kernel script...'
-    wget -O /usr/local/bin/update-kernel $UPDATE_KERNEL_URL
-    chmod +x /usr/local/bin/update-kernel
+# Define a customization script for Enipla Linux
+cat << 'EOF' > enipla-customize.sh
+#!/bin/sh
 
-    # Clone the OS resources repository
-    if [ ! -d 'os' ]; then
-        echo 'Cloning Enipla OS repository...'
-        git clone $OS_REPO os
-    fi
+# Add XFCE desktop environment, network management tools, and additional utilities
+apk add --no-cache xfce4 xfce4-terminal thunar firefox mousepad vlc evince ristretto file-roller networkmanager networkmanager-applet htop gparted xfce4-power-manager lightdm lightdm-gtk-greeter dbus
 
-    # Clone the aports repository and switch to the specified branch
-    if [ ! -d '$REPO_PATH' ]; then
-        echo 'Cloning Enipla aports repository...'
-        git clone https://github.com/Enipla/aports.git '$REPO_PATH'
-    fi
-    cd '$REPO_PATH'
-    git checkout $BRANCH
+# Enable necessary services
+rc-update add dbus
+rc-update add lightdm
+rc-update add NetworkManager
 
-    # Step 3: Modify mkimage.sh to include custom branding and software packages
-    MKIMAGE_SCRIPT='scripts/mkimage.sh'
-    echo 'Customizing \$MKIMAGE_SCRIPT...'
+# Set custom branding
+echo "Setting custom branding for Enipla..."
+echo "PRETTY_NAME=\"$CUSTOM_NAME 3.20\"" > /etc/os-release
+echo "NAME=\"$CUSTOM_NAME\"" >> /etc/os-release
+echo "ID=enipla" >> /etc/os-release
 
-    # Backup original mkimage.sh
-    cp \$MKIMAGE_SCRIPT \${MKIMAGE_SCRIPT}.bak
+# Copy branding assets
+echo "Copying Enipla branding assets..."
+mkdir -p /usr/share/pixmaps /usr/share/backgrounds /etc/lightdm
 
-    # Add branding and default software packages
-    sed -i '/^PRETTY_NAME=/c\PRETTY_NAME=\"$CUSTOM_NAME 3.20\"' \$MKIMAGE_SCRIPT
+# Copy background and logo if available
+if [ -f /mnt/data/os/enipla_background.png ]; then
+    cp /mnt/data/os/enipla_background.png /usr/share/backgrounds/enipla_background.png
+else
+    echo "Background image not found."
+fi
 
-    # Insert repository URL and software packages
-    sed -i '/^apk add --no-cache/ s/\$/ xfce4 xfce4-terminal thunar firefox mousepad vlc evince ristretto file-roller networkmanager networkmanager-applet htop gparted xfce4-power-manager lightdm/' \$MKIMAGE_SCRIPT
-    sed -i '/^# Default repository/ a REPOSITORIES=\"$REPOSITORY_URL\"' \$MKIMAGE_SCRIPT
+if [ -f /mnt/data/os/hd_enipla_logo_icon_transparent.png ]; then
+    cp /mnt/data/os/hd_enipla_logo_icon_transparent.png /usr/share/pixmaps/enipla_logo.png
+else
+    echo "Logo image not found."
+fi
 
-    # Ensure directories exist before copying files
-    mkdir -p rootfs/usr/share/pixmaps
-    mkdir -p rootfs/usr/share/backgrounds
-    mkdir -p rootfs/etc/lightdm
-    mkdir -p rootfs/etc
-    mkdir -p rootfs/boot/grub
-    mkdir -p rootfs/boot/syslinux
+# Update lightdm configuration for background
+if [ -f /etc/lightdm/lightdm-gtk-greeter.conf ]; then
+    echo "background=/usr/share/backgrounds/enipla_background.png" >> /etc/lightdm/lightdm-gtk-greeter.conf
+fi
+EOF
 
-    # Copy branding assets from the 'os' directory with checks for missing files
-    if [ -f os/hd_enipla_logo_icon_transparent.png ]; then
-        cp os/hd_enipla_logo_icon_transparent.png rootfs/usr/share/pixmaps/
-    else
-        echo 'Image hd_enipla_logo_icon_transparent.png can''t be added to /usr/share/pixmaps'
-    fi
+# Make the customization script executable
+chmod +x enipla-customize.sh
 
-    if [ -f os/enipla_background.png ]; then
-        cp os/enipla_background.png rootfs/usr/share/backgrounds/
-    else
-        echo 'Image enipla_background.png can''t be added to /usr/share/backgrounds'
-    fi
+# Run alpine-make-vm-image to create the Enipla image with GUI
+./alpine-make-vm-image \
+    -m "$REPOSITORY_URL" \
+    -b "v3.20" \
+    -s 2G \
+    -f raw \
+    -p "alpine-base openrc" \
+    -a x86_64 \
+    "$ISO_OUTPUT_DIR/$ISO_NAME" \
+    ./enipla-customize.sh
 
-    echo 'echo \"background=/usr/share/backgrounds/enipla_background.png\" >> /etc/lightdm/lightdm-gtk-greeter.conf' >> \$MKIMAGE_SCRIPT
+# Clean up
+rm enipla-customize.sh
 
-    # Update OS Information (os-release)
-    echo 'Updating OS information to \"Enipla\" in os-release...'
-    echo 'echo \"NAME=\"$CUSTOM_NAME\"\" >> rootfs/etc/os-release' >> \$MKIMAGE_SCRIPT
-    echo 'echo \"ID=enipla\" >> rootfs/etc/os-release' >> \$MKIMAGE_SCRIPT
-    echo 'echo \"PRETTY_NAME=\"$CUSTOM_NAME 3.20\"\" >> rootfs/etc/os-release' >> \$MKIMAGE_SCRIPT
-
-    # Update bootloader configuration if necessary
-    echo 'Updating bootloader configurations...'
-    echo 'sed -i \"s/Alpine/$CUSTOM_NAME/g\" rootfs/boot/grub/grub.cfg' >> \$MKIMAGE_SCRIPT
-    echo 'sed -i \"s/Alpine/$CUSTOM_NAME/g\" rootfs/boot/syslinux/syslinux.cfg' >> \$MKIMAGE_SCRIPT
-
-    # Step 4: Ensure LightDM and NetworkManager are enabled on boot
-    echo 'Configuring LightDM and NetworkManager to start on boot...'
-    echo 'rc-update add lightdm' >> \$MKIMAGE_SCRIPT
-    echo 'rc-update add networkmanager' >> \$MKIMAGE_SCRIPT
-
-    # Step 5: Run mkimage.sh with profile_extended to build the ISO
-    echo 'Building the ISO...'
-    mkdir -p '$ISO_OUTPUT_DIR'
-    chmod +x \$MKIMAGE_SCRIPT
-    ./\$MKIMAGE_SCRIPT --profile extended --repository \"$REPOSITORY_URL\" --outdir '$ISO_OUTPUT_DIR' --arch x86_64
-
-    # Step 6: Rename and move the ISO
-    ISO_FILE=\$(find \"$ISO_OUTPUT_DIR\" -name 'alpine-extended-*.iso' -print -quit)
-    if [ -f \"\$ISO_FILE\" ]; then
-        mv \"\$ISO_FILE\" \"$ISO_OUTPUT_DIR/$ISO_NAME\"
-        echo 'ISO created at $ISO_OUTPUT_DIR/$ISO_NAME'
-    else
-        echo 'ISO creation failed or ISO file not found.'
-    fi
-"
-
-echo "Exiting Alpine environment."
+echo "ISO created at $ISO_OUTPUT_DIR/$ISO_NAME"

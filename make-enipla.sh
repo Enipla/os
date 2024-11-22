9b7e9be1efcd8542930c34374c0b6203f225d879
Sectly <67114413+Sectly@users.noreply.github.com>
2024-11-22 15:34:12 +0100

    Update make-enipla.sh

--- make-enipla.sh
+++ make-enipla.sh
@@ -11,23 +11,37 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 LOGO_PATH="$SCRIPT_DIR/hd_enipla_logo_icon_transparent.png"
 BACKGROUND_PATH="$SCRIPT_DIR/enipla_background.png"
 
-# --- No need to enter chroot, as the script will be executed inside it ---
-
 # Update package lists
 apt-get update
 
-# Install LXQT, LXQT Core, and LightDM
-apt-get install -y lxqt lxqt-core lightdm
+# Install LXQT, LXQT Core, LightDM and NeoFetch
+apt-get install -y lxqt lxqt-core lightdm neofetch
 
-# Branding changes
+# --- Branding changes ---
+
+# Edit /etc/motd
+echo "Welcome to Enipla \"Begone\" OS" > /etc/motd
+echo "" >> /etc/motd
+echo "An OS powered by hopes and dreams" >> /etc/motd
+
+# Edit /etc/issue
 sed -i "s/Debian GNU\/Linux/Enipla Begone/g" /etc/issue
+
+# Edit /etc/os-release
 sed -i "s/Debian GNU\/Linux/Enipla Begone/g" /etc/os-release
+sed -i "s/PRETTY_NAME=\"Debian GNU\/Linux/PRETTY_NAME=\"Enipla Begone/g" /etc/os-release
+sed -i "s/NAME=\"Debian GNU\/Linux/NAME=\"Enipla Begone/g" /etc/os-release
+
+# Change hostname
+echo "enipla" > /etc/hostname
+hostnamectl set-hostname enipla
 
 # --- LightDM Configuration ---
 # Set the background image
 sed -i "s/#background=/background=$BACKGROUND_PATH/g" /etc/lightdm/lightdm-gtk-greeter.conf
 
 # --- LXQT Configuration ---
-# Set the logo (replace 'lxqt-panel' with your actual theme name if different)
 mkdir -p /usr/share/lxqt/themes/lxqt-panel/
 cp "$LOGO_PATH" /usr/share/lxqt/themes/lxqt-panel/logo.png
+
+neofetch
